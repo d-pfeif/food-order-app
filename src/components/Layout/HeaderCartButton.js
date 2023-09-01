6a804d364c8ba939267fadcadb2c771891bdc565
Dakota Pfeifer <d-pfeif@outlook.com>
2023-08-31 22:40:03 -0700

    add useEffect to add css animation when items are added or removed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import classes from "./HeaderCartButton.module.css"
 import CartIcon from "../Cart/CartIcon"
 import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
+  const [buttonActive, setButtonActive] = useState(false)
   const cartCtx = useContext(CartContext)
+  const {items} = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+
+  const numberOfCartItems = items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${buttonActive ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setButtonActive(true)
+
+    const timer = setTimeout(() => {
+      setButtonActive(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
